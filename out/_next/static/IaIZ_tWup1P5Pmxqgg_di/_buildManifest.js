02d4fa1c331bdde37e685e91f58fd335542a26fe
Oscar Mairey <o@mairey.net>
2021-12-29 18:30:20 +0100

    nodejs

---
+++ out/_next/static/IaIZ_tWup1P5Pmxqgg_di/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/78e521c3-5a5c77cfbeb29b59.js","static/css/1a2fa8d50adebc58.css","static/chunks/pages/index-329d2afc7a61e60b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
